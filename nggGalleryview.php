c96c577ac58466df1bee1822938a7308af65640c
Brandon Hubbard <bkhubbard@gmail.com>
2012-11-02 17:34:06 -0700

    Adding Plugin Files

---
+++ nggGalleryview.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: NextGEN Galleryview Test
+Plugin URI: https://github.com/bhubbard/NextGEN-Galleryview
+Description: Add the script files and template for the jQuery Plugin Galleryview integration from Jack Anderson (http://www.spaceforaname.com/galleryview/). Use the shortcode [nggallery id=x template="galleryview"] to show the new layout.
+Author: Alex Rabe, Brandon Hubbard
+Author URI: http://brandonhubbard.com/
+Version: 1.1
+*/
+
+if (!class_exists('nggGalleryview')) {
+	class nggGalleryview {
+
+		var $plugin_url = false;
+
+		function nggGalleryview() {
+
+			// Define Plugin URL
+			$this->plugin_url = WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ) . '/';
+
+			// Load Scripts, Styles, and Templates
+			add_action('wp_print_scripts', array(&$this, 'ngg_galleryview_scripts') );
+			add_action('wp_print_styles', array(&$this, 'ngg_galleryview_styles') );
+			add_filter('ngg_render_template', array(&$this, 'add_template'), 10, 2);
+		}
+
+		// Add our Template
+		function add_template( $path, $template_name = false) {
+			if ($template_name == 'gallery-galleryview')
+				$path = WP_PLUGIN_DIR . '/' . plugin_basename( dirname(__FILE__) ) . '/view/gallery-galleryview.php';
+			return $path;
+		}
+
+		// GalleryView Styles
+		function ngg_galleryview_styles() {
+			if ( !is_admin() ) { // we do not want our styles to load in the dashboard
+				wp_enqueue_style('galleryview', $this->plugin_url . '/galleryview/css/galleryview.css', false, null, 'all');
+			}
+		}
+
+		// GalleryView Scripts
+		function ngg_galleryview_scripts() {
+
+			if ( !is_admin() ) { // we do not want our scripts to load in the dashboard
+
+				// jQuery Easing via CDN
+				wp_register_script('jquery-easing', 'http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js', 'jquery', null, false);
+				wp_enqueue_script('jquery-easing');
+
+				// jQuery Timers
+				wp_register_script('jquery-timers', $this->plugin_url . '/galleryview/js/jquery.timers.js', 'jquery', null, false);
+				wp_enqueue_script('jquery-timers');
+
+				// jQuery GalleryView
+				wp_register_script('jquery-galleryview', $this->plugin_url . '/galleryview/js/jquery.galleryview.js', array('jquery', 'jquery-timers', 'jquery-easing'), null, false);
+				wp_enqueue_script('jquery-galleryview');
+			}
+		}
+	}
+
+	// Start this plugin once all other plugins are fully loaded
+	add_action( 'plugins_loaded', create_function( '', 'global $nggGalleryview; $nggGalleryview = new nggGalleryview();' ) );
+
+}
---
+++ view/gallery-galleryview.php
@@ -0,0 +1,79 @@
+<?php 
+/**
+Template Page for the jQuery Galleryview integration
+
+Follow variables are useable :
+
+	$gallery     : Contain all about the gallery
+	$images      : Contain all images, path, title
+	$pagination  : Contain the pagination content
+
+ You can check the content when you insert the tag <?php var_dump($variable) ?>
+ If you would like to show the timestamp of the image ,you can use <?php echo $exif['created_timestamp'] ?>
+**/
+
+?>
+<?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?><?php if (!empty ($gallery)) : ?>
+
+<div id="<?php echo $gallery->anchor ?>" class="galleryview">
+	<!-- Thumbnails -->
+	<?php foreach ($images as $image) : ?>		
+	<div class="panel">
+		<a href="<?php echo $image->imageURL ?>" title="<?php echo $image->alttext ?>" rel="lightbox"><img src="<?php echo $image->imageURL ?>" alt="<?php echo $image->alttext ?>" /></a>
+	</div>
+ 	<?php endforeach; ?>
+  	<ul class="filmstrip">
+  	<?php foreach ($images as $image) : ?>	
+	    <li><div class="filmstrip-borders"></div><img src="<?php echo $image->thumbnailURL ?>" alt="<?php echo $image->alttext ?>" title="<?php echo $image->alttext ?>" /></li>
+	<?php endforeach; ?>
+  	</ul>
+
+</div>
+<div class="clear"><br /></div>
+<script type="text/javascript" defer="defer">
+	jQuery("document").ready(function(){
+		jQuery('#<?php echo $gallery->anchor ?>').galleryView({
+		
+		// General Options
+		transition_speed: 1000, 		//INT - duration of panel/frame transition (in milliseconds)
+		transition_interval: 5000, 		//INT - delay between panel/frame transitions (in milliseconds)
+		easing: 'swing', 				//STRING - easing method to use for animations (jQuery provides 'swing' or 'linear', more available with jQuery UI or Easing plugin)
+		
+		// Panel Options
+		show_panels: true, 				//BOOLEAN - flag to show or hide panel portion of gallery
+		show_panel_nav: true, 			//BOOLEAN - flag to show or hide panel navigation buttons
+		enable_overlays: false, 			//BOOLEAN - flag to show or hide panel overlays
+		panel_width: 600, 				//INT - width of gallery panel (in pixels)
+		panel_height: 300, 				//INT - height of gallery panel (in pixels)
+		panel_animation: 'fade', 		//STRING - animation method for panel transitions (crossfade,fade,slide,none)
+		panel_scale: 'crop', 			//STRING - cropping option for panel images (crop = scale image and fit to aspect ratio determined by panel_width and panel_height, fit = scale image and preserve original aspect ratio)
+		overlay_position: 'bottom', 	//STRING - position of panel overlay (bottom, top)
+		pan_images: false,				//BOOLEAN - flag to allow user to grab/drag oversized images within gallery
+		pan_style: 'drag',				//STRING - panning method (drag = user clicks and drags image to pan, track = image automatically pans based on mouse position
+		pan_smoothness: 15,				//INT - determines smoothness of tracking pan animation (higher number = smoother)
+		
+		// Filmstrip Options
+		start_frame: 1, 				//INT - index of panel/frame to show first when gallery loads
+		show_filmstrip: true, 			//BOOLEAN - flag to show or hide filmstrip portion of gallery
+		show_filmstrip_nav: true, 		//BOOLEAN - flag indicating whether to display navigation buttons
+		enable_slideshow: true,			//BOOLEAN - flag indicating whether to display slideshow play/pause button
+		autoplay: false,				//BOOLEAN - flag to start slideshow on gallery load
+		show_captions: false, 			//BOOLEAN - flag to show or hide frame captions	
+		filmstrip_size: 3, 				//INT - number of frames to show in filmstrip-only gallery
+		filmstrip_style: 'scroll', 		//STRING - type of filmstrip to use (scroll = display one line of frames, scroll filmstrip if necessary, showall = display multiple rows of frames if necessary)
+		filmstrip_position: 'bottom', 	//STRING - position of filmstrip within gallery (bottom, top, left, right)
+		frame_width: 80, 				//INT - width of filmstrip frames (in pixels)
+		frame_height: 40, 				//INT - width of filmstrip frames (in pixels)
+		frame_opacity: 0.4, 			//FLOAT - transparency of non-active frames (1.0 = opaque, 0.0 = transparent)
+		frame_scale: 'crop', 			//STRING - cropping option for filmstrip images (same as above)
+		frame_gap: 5, 					//INT - spacing between frames within filmstrip (in pixels)
+		
+		// Info Bar Options
+		show_infobar: true,				//BOOLEAN - flag to show or hide infobar
+		infobar_opacity: 1				//FLOAT - transparency for info bar
+		});
+	});
+	
+</script>
+<?php endif; ?>
+
