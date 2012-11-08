bf04198749448eaa00a693a3286ef8e707714808
Brandon Hubbard <bkhubbard@gmail.com>
2012-11-07 17:13:52 -0800

    Adding image size

--- view/gallery-galleryview.php
+++ view/gallery-galleryview.php
@@ -30,7 +30,7 @@ If you wish to modify this template please create a folder called nggallery with
 	<!-- Thumbnails -->
 	<?php foreach ($images as $image) : ?>
 	<div class="panel">
-		<img src="<?php echo $image->imageURL ?>" alt="<?php echo $image->alttext ?>" title="<?php echo $image->alttext ?>" data-description="<?php echo $image->description ?>" />
+		<img src="<?php echo $image->imageURL ?>" alt="<?php echo $image->alttext ?>" title="<?php echo $image->alttext ?>" data-description="<?php echo $image->description ?>" <?php echo $image->size ?> />
 	</div>
  	<?php endforeach; ?>
   	<ul class="filmstrip">
