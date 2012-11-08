cbd9124ad8740acf2172e9b3dadeb8433e6da7a1
Brandon Hubbard <bkhubbard@gmail.com>
2012-11-07 17:50:52 -0800

    Updated version number

--- nggGalleryview.php
+++ nggGalleryview.php
@@ -5,7 +5,7 @@ Plugin URI: http://bhubbard.github.com/NextGEN-Galleryview/
 Description: Add the script files and template for the jQuery Plugin Galleryview integration from Jack Anderson (http://www.spaceforaname.com/galleryview/). Use the shortcode [nggallery id=x template="galleryview"] to show the new layout. Plugin originally created by Alex Rabe.
 Author: Alex Rabe, Brandon Hubbard
 Author URI: http://brandonhubbard.com/
-Version: 1.1.8
+Version: 1.2
 */
 
 if (!class_exists('nggGalleryview')) {
